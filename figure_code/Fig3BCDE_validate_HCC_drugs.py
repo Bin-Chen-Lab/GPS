b749f1d3cafcd991bacd5b85aa35da718e32cdec
Dmitry Leshchiner <64936858+mlesch1994@users.noreply.github.com>
2025-03-07 15:53:34 -0500

    Add files via upload
    Code used to generate figures.

---
+++ figure_code/Fig3BCDE_validate_HCC_drugs.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 21:07:49 2020
+
+@author: jingxing
+"""
+
+import pandas as pd
+from scipy.stats import ranksums, spearmanr, zscore, rankdata, beta
+import numpy as np
+np.random.seed(1)
+from sklearn.metrics import roc_auc_score, roc_curve
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+matplotlib.rcParams["font.family"] = "Arial"
+import seaborn as sns
+from multiprocessing import Pool
+
+    
+
+def score_norm_rges(gene_up, gene_down, dz_de, bgrd):
+    bgrd_up, bgrd_down = bgrd
+    up_tags_position = dz_de.loc[gene_up].sort_values(by='RANK').RANK
+    down_tags_position = dz_de.loc[gene_down].sort_values(by='RANK').RANK
+    
+    if len(gene_up) >= 2:
+        a_up = max(map(lambda x: x/len(gene_up) - up_tags_position[x-1]/dz_de.shape[0], \
+                       range(1, len(gene_up)+1)))
+        b_up = max(map(lambda x: up_tags_position[x-1]/dz_de.shape[0] - (x-1)/len(gene_up), \
+                       range(1, len(gene_up)+1)))
+        ks_up = a_up if a_up > b_up else -b_up
+    else:
+        ks_up = 0
+    if len(gene_down) >= 2:
+        a_down = max(map(lambda x: x/len(gene_down) - down_tags_position[x-1]/dz_de.shape[0], \
+                       range(1, len(gene_down)+1)))
+        b_down = max(map(lambda x: down_tags_position[x-1]/dz_de.shape[0] - (x-1)/len(gene_down), \
+                       range(1, len(gene_down)+1)))
+        ks_down = a_down if a_down > b_down else -b_down
+    else:
+        ks_down = 0
+    z_up = zscore([ks_up] + bgrd_up)[0]
+    z_down = zscore([ks_down] + bgrd_down)[0]
+    score = z_up - z_down
+    return score, z_up, z_down
+
+def score_rges(gene_up, gene_down, dz_de):
+    up_tags_position = dz_de.loc[gene_up].sort_values(by='RANK').RANK
+    down_tags_position = dz_de.loc[gene_down].sort_values(by='RANK').RANK
+    
+    if len(gene_up) >= 2:
+        a_up = max(map(lambda x: x/len(gene_up) - up_tags_position[x-1]/dz_de.shape[0], \
+                       range(1, len(gene_up)+1)))
+        b_up = max(map(lambda x: up_tags_position[x-1]/dz_de.shape[0] - (x-1)/len(gene_up), \
+                       range(1, len(gene_up)+1)))
+        ks_up = a_up if a_up > b_up else -b_up
+    else:
+        ks_up = 0
+    if len(gene_down) >= 2:
+        a_down = max(map(lambda x: x/len(gene_down) - down_tags_position[x-1]/dz_de.shape[0], \
+                       range(1, len(gene_down)+1)))
+        b_down = max(map(lambda x: down_tags_position[x-1]/dz_de.shape[0] - (x-1)/len(gene_down), \
+                       range(1, len(gene_down)+1)))
+        ks_down = a_down if a_down > b_down else -b_down
+    else:
+        ks_down = 0
+    return ks_up - ks_down, ks_up, ks_down
+
+
+
+def penalty_pct(profile, dz_de_ori):
+    dz_de = dz_de_ori[dz_de_ori.index.isin(profile.index)]
+    dz_gene_up = set(dz_de[dz_de['Value'] > 0].index)
+    dz_gene_down = set(dz_de[dz_de['Value'] < 0].index)
+    gene_up = set(profile.loc[profile == 1].index)
+    gene_down = set(profile.loc[profile == -1].index)
+    p_up = len(set.intersection(gene_up, dz_gene_up)) / len(dz_gene_up)
+    p_down = len(set.intersection(gene_down, dz_gene_down)) / len(dz_gene_down)
+    return (p_up + p_down) / 2, len(gene_up), len(gene_down)
+
+
+def profile_score(params):
+    cpd, profile, dzde, transcrptm, permt_rges = params
+    up_genes = sorted(set(profile.loc[profile == 1].index))
+    down_genes = sorted(set(profile.loc[profile == -1].index))
+    
+    bins_up = [int(b.split('_')[-1]) for b in permt_rges.index if b.startswith('Up_')]
+    bins_down = [int(b.split('_')[-1]) for b in permt_rges.index if b.startswith('Down_')]
+    mark_up = min(bins_up, key=lambda x: abs(x - len(up_genes)))
+    mark_down = min(bins_down, key=lambda x: abs(x - len(down_genes)))
+    bgrd_up = permt_rges.loc['Up_%s'%mark_up].to_list()
+    bgrd_down = permt_rges.loc['Down_%s'%mark_down].to_list()
+    rges_bgrd = (bgrd_up, bgrd_down)
+    zrges, z_up, z_down = score_norm_rges(up_genes, down_genes, dzde, rges_bgrd)
+    penalty, num_up, num_down = penalty_pct(profile, dzde)
+    rges, ks_up, ks_down = score_rges(up_genes, down_genes, dzde)
+    return cpd, zrges, z_up, z_down, rges, penalty, num_up, num_down
+
+
+        
+### Working directory and input files
+### Use high-throughput dataset to evaluate enrichment, manually curated dataset to evalutate correlation.
+wrkdir = '../data/HCC_efficacy_rges/'
+
+com_roc = pd.read_hdf(wrkdir + 'HCC_CTRP_MEDIAN_mlp_2kGenes_preds_0.95.hdf5').T
+com_cor = pd.read_hdf(wrkdir + 'HCC_drugs_MEDIAN_mlp_2kGenes_preds_0.95.hdf5').T
+
+dzde = pd.read_csv(wrkdir + 'DZSIG__HCC_opt.csv', index_col='GeneSymbol')
+dzde['RANK'] = dzde['Value'].rank(ascending=False)
+permt_bgrd = pd.read_csv(wrkdir + 'BGRD__HCC_opt.csv', index_col=0)
+
+meta_cor = pd.read_csv(wrkdir + 'HCC_drug_efficacy.csv', index_col=0)
+meta_roc = pd.read_csv(wrkdir + 'CTRP_HEPG2_efficacy.csv', index_col='cpd_name')
+
+
+### Define the transcriptome
+rdc_transcrptm = set.intersection(set(com_cor.columns), set(com_roc.columns), set(dzde.index))
+
+### Align compound profiles, activity information and disease signatures
+com_cor = com_cor.loc[:, com_cor.columns.isin(rdc_transcrptm)]
+com_roc = com_roc.loc[:, com_roc.columns.isin(rdc_transcrptm)]
+com_roc = com_roc.loc[com_roc.index.isin(meta_roc.index)]
+meta_cor = meta_cor.loc[com_cor.index]
+meta_roc = meta_roc.loc[com_roc.index]
+meta_cor['pIC50'] = [-np.log10(i * 10 ** (-9)) for i in meta_cor['standard_value (nm)']]
+meta_cor['Active'] = [True if i > 5 else False for i in meta_cor['pIC50']]
+
+meta_roc['Active'] = [True if auc <= 9 else False for auc in meta_roc.area_under_curve]
+meta_roc = meta_roc.loc[(meta_roc.area_under_curve <= 9) | (meta_roc.area_under_curve >= 12)]
+meta_roc.loc['niclosamide', 'Active'] = True
+com_roc = com_roc.loc[com_roc.index.isin(meta_roc.index)]
+
+
+### Calculate reversal scores of the correlation dataset
+eva_cor = []
+for cpd, profile in com_cor.iterrows():
+    params = (cpd, profile, dzde, rdc_transcrptm, permt_bgrd)
+    res = profile_score(params)
+    eva_cor.append(list(res))
+eva_cor = pd.DataFrame(data=eva_cor, columns=['DrugName', 'Z_RGES', 'Z_Up', 'Z_Down', 'Raw_RGES', 'Penalty', 'Num_Up', 'Num_Down'])
+eva_cor.index = eva_cor.iloc[:, 0]
+eva_cor = eva_cor.iloc[:, 1:]
+eva_cor = pd.concat([meta_cor, eva_cor], axis=1)
+
+
+### Calculate reversal scores of the enrichment dataset
+eva_roc = []
+for cpd, profile in com_roc.iterrows():
+    params = (cpd, profile, dzde, rdc_transcrptm, permt_bgrd)
+    res = profile_score(params)
+    eva_roc.append(list(res))
+eva_roc = pd.DataFrame(data=eva_roc, columns=['DrugName', 'Z_RGES', 'Z_Up', 'Z_Down', 'Raw_RGES', 'Penalty', 'Num_Up', 'Num_Down'])
+eva_roc.index = eva_roc.iloc[:, 0]
+eva_roc = eva_roc.iloc[:, 1:]
+eva_roc = pd.concat([meta_roc, eva_roc], axis=1)
+eva_roc['Active'] = eva_roc['Active'].fillna(False)
+#eva_roc = eva_roc[(eva_roc['Num_Up'] >= 5) & (eva_roc['Num_Down'] >= 5)]
+
+FS = (3.5, 3.5)
+### Evaluate correlation: pIC50 ~ reversal scores
+eva_cor_tstd = eva_cor.loc[~eva_cor.pIC50.isna()]
+Xs, Ys = eva_cor_tstd['pIC50'], eva_cor_tstd['Z_RGES']
+FIG = plt.figure(figsize=FS)
+plt.plot(Xs, Ys, 'o', color='black')
+plt.xlabel('pIC50')
+plt.ylabel('Z-RGES')
+r, p = spearmanr(Xs, Ys)
+plt.text(7.5, -2.5, 'Cor = %.3f\nP = %.4f'%(r, p), size=12, weight='bold')
+for drug, row in eva_cor_tstd.iterrows():
+    x, y = row['pIC50'], row['Z_RGES']
+    plt.text(x, y, drug, color='grey')
+plt.tight_layout()
+#FIG.savefig('../doc_fig_table/HCCinSilico_zrges_pIC50.pdf', transparent=True)
+
+Ys = eva_cor_tstd['Raw_RGES']
+FIG = plt.figure(figsize=FS)
+plt.plot(Xs, Ys, 'o', color='cornflowerblue')
+plt.xlabel('pIC50')
+plt.ylabel('Raw RGES')
+r, p = spearmanr(Xs, Ys)
+plt.text(7.5, -1.1, 'Cor = %.3f\nP = %.4f'%(r, p), size=12, weight='bold')
+for drug, row in eva_cor_tstd.iterrows():
+    x, y = row['pIC50'], row['Raw_RGES']
+    plt.text(x, y, drug, color='grey')
+plt.tight_layout()
+#FIG.savefig('../doc_fig_table/HCCinSilico_rges_pIC50.pdf', transparent=True)
+
+
+### Evaluate AUC-ROC: area_under_curve ~ reversal scores
+y_true = eva_roc.Active.to_list()
+ratio = sum(y_true) / len(y_true)
+score_zrges = -eva_roc['Z_RGES']
+auc_zrges = roc_auc_score(y_true, score_zrges)
+fprz, tprz, thrz = roc_curve(y_true, score_zrges)
+score_rges = -eva_roc['Raw_RGES']
+auc_rges = roc_auc_score(y_true, score_rges)
+fpr, tpr, thr = roc_curve(y_true, score_rges)
+
+FIG = plt.figure(figsize=FS)    
+plt.plot(fprz, tprz, color='black', label='Z-RGES AU-ROC = %.3f'%auc_zrges)
+plt.plot(fpr, tpr, color='cornflowerblue', label='Raw RGES AU-ROC = %.3f'%auc_rges)
+plt.text(0, 0.9, 'CTRP HepG2\n%s compounds'%eva_roc.shape[0], size=12, weight='bold')
+plt.legend()
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.tight_layout()
+#FIG.savefig('../doc_fig_table/HCCinSilico_ROC_zrges_rges.pdf', transparent=True)
+
+topn = 100        
+ranking_zrges = sorted([(s, yt) for s, yt in zip(score_zrges, y_true)], reverse=True)
+prcs_zrges = [sum([r[1] for r in ranking_zrges[:i]]) / i for i in range(1, topn+1)]
+scr_zrges = range(1, topn+1)
+ranking_rges = sorted([(s, yt) for s, yt in zip(score_rges, y_true)], reverse=True)
+prcs_rges = [sum([r[1] for r in ranking_rges[:i]]) / i for i in range(1, topn+1)]
+scr_rges = range(1, topn+1)
+
+FIG = plt.figure(figsize=FS)
+plt.plot(scr_zrges, prcs_zrges, color='black', label='Z-RGES')
+plt.plot(scr_rges, prcs_rges, color='cornflowerblue', label='Raw RGES')
+plt.plot([0.0, topn], [ratio, ratio], '--', color='grey', label='Overal positive ratio')
+plt.legend()
+plt.xlabel('Rank')
+plt.ylabel('Hit Rate')
+plt.ylim(0.0, 1.05)
+plt.text(20, 0.5, 'CTRP HepG2\n%s compounds'%eva_roc.shape[0], size=12, weight='bold')
+plt.tight_layout()
+#FIG.savefig('../doc_fig_table/HCCinSilico_HitRate_zrges_rges.pdf', transparent=True)
+    
+print('Number of actives: %s\nActive ratio: %s'%(sum(y_true), ratio))
+
+
+### Compare hit rate of octad compounds and unseen compounds in PRISM
+eff = pd.read_csv(wrkdir + 'PRISM_HUH7_efficacy.csv', index_col=0)
+eff = eff.loc[~eff['Active'].isna()]
+srges = pd.read_csv(wrkdir + 'sRGES_HCC_octad.csv', index_col='pert_iname')
+zrges = pd.read_csv(wrkdir + 'PRISM_HCC_opt_RGES_norm.csv', index_col=0)
+eff['sRGES'] = srges.reindex(eff.index)['sRGES']
+eff['Z_RGES'] = zrges.reindex(eff.index)['Z_RGES']
+eff_sub_s = eff.loc[~eff['sRGES'].isna()]
+eff_sub_z = eff.loc[(~eff.index.isin(eff_sub_s.index)) & (~eff['Z_RGES'].isna())]
+
+topn = 100
+ranking_srges = sorted([(s, yt) for s, yt in zip(eff_sub_s['sRGES'], eff_sub_s['Active'])])
+prcs_srges = [sum([r[1] for r in ranking_srges[:i]]) / i for i in range(1, topn+1)]
+scr_srges = range(1, topn+1)
+ratio_srges = eff_sub_s['Active'].mean()
+ranking_zrges = sorted([(s, yt) for s, yt in zip(eff_sub_z['Z_RGES'], eff_sub_z['Active'])])
+prcs_zrges = [sum([r[1] for r in ranking_zrges[:i]]) / i for i in range(1, topn+1)]
+scr_zrges = range(1, topn+1)
+ratio_zrges = eff_sub_z['Active'].mean()
+
+FIG = plt.figure(figsize=FS)
+plt.plot(scr_srges, prcs_srges, color='cornflowerblue', label='sRGES %s OCTAD cpds'%eff_sub_s.shape[0])
+plt.plot(scr_zrges, prcs_zrges, color='black', label='Z-RGES %s other cpds'%eff_sub_z.shape[0])
+plt.plot([0.0, topn], [ratio_srges, ratio_srges], '--', color='cornflowerblue', \
+         label='OCTAD cpds pos ratio')
+plt.plot([0.0, topn], [ratio_zrges, ratio_zrges], '--', color='black', \
+         label='Other cpds pos ratio')
+plt.text(topn*0.6, 0.5, 'PRISM Huh7', size=12, weight='bold')
+plt.legend()
+plt.xlabel('Rank')
+plt.ylabel('Hit Rate')
+plt.ylim(0.0, 1.05)
+plt.tight_layout()
+#FIG.savefig('../doc_fig_table/HCCinSilico_HitRate_zrges_srges.pdf', transparent=True)
+
+
+
+
+
+
+
+
+
+
---
+++ figure_code/Fig5CDE_search_GSAR.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  6 21:04:58 2024
+
+@author: xingjin1
+"""
+import pandas as pd
+import numpy as np
+import os
+from scipy.stats import ranksums, spearmanr
+import matplotlib
+matplotlib.rcParams['figure.dpi'] = 300
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+matplotlib.rcParams["font.family"] = "Arial"
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+wrkdir = '../data/GSAR/'
+rnaseq_dir = wrkdir + 'rna-seq/'
+cpd_sigs = pd.read_csv(wrkdir + 'HCC_cmpds_gps_2types.csv', index_col=0)
+cpd_info = pd.read_csv(wrkdir + 'DataSx_HCC_SAR_2types_cluster.csv', index_col=0)
+cpd_info = cpd_info.loc[cpd_info['Good_pred'] == 'Yes']
+cpd_info = cpd_info.sort_values(by=['Chemical Type', 'pIC50 Summary'], ascending=[True, False])
+ovlp = [cpd for cpd in cpd_info.index if cpd in cpd_sigs.columns]
+cpd_info = cpd_info.loc[ovlp]
+cpd_sigs = cpd_sigs.loc[:, ovlp]
+'''
+keep = cpd_sigs.where(cpd_sigs != 0).count(axis=1)
+keep = keep.loc[keep >= 2].index
+cpd_sigs = cpd_sigs.loc[keep]
+'''
+print(cpd_sigs.shape)
+
+tst = ['PB56874852', '5260420', '45302']
+#tst += ['44443110', '45300']
+trn = [cpd for cpd in cpd_info.index if not cpd in tst]
+cpd_sigs_trn = cpd_sigs.loc[:, trn]
+eff_trn = cpd_info.loc[trn, 'pIC50 Summary']
+
+result = []
+for gene, row in cpd_sigs_trn.iterrows():
+    ct = row.value_counts()
+    es, p, d, good = np.nan, np.nan, 0, False
+    if len(ct) == 3:    
+        es, p = spearmanr(row, eff_trn, nan_policy='omit')
+        if p < 0.005:
+        #if round(p, 2) <= 0.01:
+            d = 1 if es > 0 else -1
+            good = True
+    elif len(ct) == 2:
+        t = row.loc[row == ct.index.min()].index
+        a = eff_trn.loc[t]
+        t = row.loc[row == ct.index.max()].index
+        b = eff_trn.loc[t]
+        es, p = ranksums(a, b, nan_policy='omit')
+        #if round(p, 2) <= 0.01:
+        if p < 0.005:
+            d = 1 if es < 0 else -1
+            good = True
+    else: continue
+    result.append([gene, len(ct), es, p, good, d])
+        
+result = pd.DataFrame(data=result, columns=['Gene', 'Cats', 'ES', 'P', 'Selected', 'Direction'])
+result.index = result['Gene']
+result = result.iloc[:, 1:]
+result = result.sort_values(by=['P'])
+sele = result.loc[result['Selected'] == True].index.to_list()
+print(len(sele))
+
+viz = []
+file_list = ['DE_MSU45302.csv', 'DE_4852_10uM.csv', 'DE_4852_4uM.csv']
+for fn in file_list:
+    treat = fn.replace('DE_', '').replace('.csv', '').replace('_', ' ')
+    if 'Niclosamide' in treat: continue
+    if '5260420' in treat: continue
+    de = pd.read_csv(rnaseq_dir + fn, index_col='Symbol')
+    de = de.loc[~de.index.isna()]
+    de['abs'] = de['log2FoldChange'].abs()
+    de = de.sort_values(by='abs', ascending=False)
+    de = de.loc[~de.index.duplicated()]
+    #de['log2FoldChange'] = [round(lfc, 2) if p < 0.2 else 0 for lfc, p in zip(de['log2FoldChange'], de['padj'])]
+    
+    cal = result.loc[(result.index.isin(de.index)) & (result['Selected'] == True), ['Direction']]
+    cal['log2FoldChange'] = de.loc[cal.index, 'log2FoldChange']
+    cal['decomposition'] = cal['log2FoldChange'] * cal['Direction']
+    score = cal['decomposition'].sum() / len(sele)
+    
+    #tmp = cal['log2FoldChange'].reindex(sele).fillna(0).round(2)
+    tmp = de.reindex(sele)[['pvalue', 'log2FoldChange']]
+    t = []
+    for p, lfc in zip(tmp['pvalue'], tmp['log2FoldChange']):
+        if np.isnan(p):
+            t.append(0)
+        elif lfc > 0:
+            t.append(-np.log10(p))
+        else:
+            t.append(np.log10(p))
+    tmp[treat] = t
+    viz.append(tmp[treat].round(2))
+    
+    print(treat, score)
+viz = pd.concat(viz, axis=1)
+viz = viz.sort_values(by='MSU45302')
+#viz['Required'] = np.log10(result.loc[viz.index, 'P'])
+
+FIG = plt.figure(figsize=(3, 6))
+sns.heatmap(viz, vmin=-2, center=0, vmax=2, cmap='coolwarm', cbar=False, annot=True, linewidths=1, linecolor='grey')
+plt.ylabel('Efficacy responsive genes', fontsize=12)
+plt.xticks(rotation=45, ha='right')
+plt.title('Direction & Significance\n(RNA-seq)')
+plt.tight_layout()
+plt.savefig(wrkdir + 'Heatmap_GSAR-sig_302-4852.png', transparent=True)
+
+
+gm = cpd_sigs.loc[sele, cpd_info.index].T
+mp = {-1: 'Down', 0: 'None', 1: 'Up'}
+for col in gm.columns:
+    if col == 'pIC50 Summary': continue
+    gm[col] = [mp[r] for r in gm[col]]
+gm = pd.concat([cpd_info['pIC50 Summary'], gm], axis=1)
+gm = gm.loc[gm['pIC50 Summary'] > 0]
+
+
+genes2show = ['WDR75', 'KIF23', 'UHRF1', 'MCM6']
+clr_code = {'Down': 'cornflowerblue', 'None': 'silver', 'Up': 'Salmon'}
+for g in genes2show:
+    gm = gm.sort_values(by=g)
+    fig = plt.figure(figsize=(2.4, 2.4))
+    sns.boxplot(data=gm, x=g, y='pIC50 Summary', palette=clr_code)
+    sns.swarmplot(data=gm, x=g, y='pIC50 Summary', color='black')
+    #plt.ylabel(None)
+    plt.xlabel(None)
+    plt.title(g + ': P = %.4f'%result.loc[g, 'P'])
+    plt.tight_layout()
+    plt.savefig(wrkdir + 'pIC50_separation_%s.png'%g, transparent=True)
+
+
+de = pd.read_csv('../data/GSAR/rna-seq/DE_MSU45302.csv', index_col='Symbol')
+de = de.loc[~de.index.isna()]
+de['abs'] = de['log2FoldChange'].abs()
+de = de.sort_values(by='abs', ascending=False)
+c302 = de.loc[~de.index.duplicated()]
+de = pd.read_csv('../data/GSAR/rna-seq/DE_4852_10uM.csv', index_col='Symbol')
+de = de.loc[~de.index.isna()]
+de['abs'] = de['log2FoldChange'].abs()
+de = de.sort_values(by='abs', ascending=False)
+c4852 = de.loc[~de.index.duplicated()]
+com = pd.concat([c302[['log2FoldChange', 'padj']], c4852[['log2FoldChange', 'padj']]], axis=1)
+com.columns = ['log2FoldChange_MSU45302', 'padj_MSU45302', 'log2FoldChange_4852', 'padj_4852']
+com = com.reindex(cpd_sigs.index).dropna()
+tmp, thr = [], 0.1
+for a, b in zip(com['padj_MSU45302'], com['padj_4852']):
+    tmp.append(sum([i < thr for i in [a, b]]))
+com['Significant'] = tmp
+com = com.sort_values(by='log2FoldChange_MSU45302', ascending=False)
+
+clr_code = {0: 'grey', 1: 'salmon', 2: 'chocolate'}
+fig = plt.figure(figsize=(4, 4))
+sns.scatterplot(data=com, x='log2FoldChange_MSU45302', y='log2FoldChange_4852', \
+                hue='Significant', palette=clr_code, alpha=0.75, size='Significant')
+for key, row in com.loc[com['Significant'] == 2].iterrows():
+    a, b = row['log2FoldChange_MSU45302'], row['log2FoldChange_4852']
+    plt.text(a, b, key)
+plt.xlim(-1, 1)
+plt.ylim(-3, 3)
+plt.show()
+
+    
+
+
+
+
