78245c83e5d5e31027dfb47f24b158359fc33679
Dmitry Leshchiner <64936858+mlesch1994@users.noreply.github.com>
2026-03-16 15:28:53 -0400

    Update OCTAD_IPF.R

--- Tutorial/IPF/OCTAD_IPF.R
+++ Tutorial/IPF/OCTAD_IPF.R
@@ -7,4 +7,5 @@ sRGES = runsRGES(data, max_gene_size=100, permutations=10000)
 sRGES = sRGES[sRGES$sRGES < -0.2,]
 sRGES = sRGES[!grepl("BRD-", sRGES$pert_iname),]
 
-write.csv('octad_MUC5B+.csv', srges_nom)
+print(sRGES)
+write.csv(sRGES, 'octad_MUC5B+.csv')
